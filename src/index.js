6e41e215a57e62161d9da673541d96ee38875e66
Zachary Dovel <zakkarido@gmail.com>
2018-09-10 09:02:09 -0700

    Update documentation and build system

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 /**
+ * @module @zakkudo/actions
+ */
+
+/**
  * @param {String} text - The text to uncapitalize the first letter of
  * @return {String} The uncapitalized version of text
  * @private
@@ -110,82 +114,13 @@ function addActionTypeNamesWithScopes(actionCreators, namespace, self) {
     }, {});
 }
 
-/**
- * Helper class to make working with [Redux]{@link https://redux.js.org/} actions enjoyable. The code is readable,
- * with minimal surprises.
- *
- * <p>
- * <a href="https://travis-ci.org/zakkudo/actions">
- *     <img src="https://travis-ci.org/zakkudo/actions.svg?branch=master"
- *          alt="Build Status" /></a>
- * <a href="https://coveralls.io/github/zakkudo/actions?branch=master">
- *     <img src="https://coveralls.io/repos/github/zakkudo/actions/badge.svg?branch=master"
- *          alt="Coverage Status" /></a>
- * <a href="https://snyk.io/test/github/zakkudo/actions">
- *     <img src="https://snyk.io/test/github/zakkudo/actions/badge.svg"
- *          alt="Known Vulnerabilities"
- *          data-canonical-src="https://snyk.io/test/github/zakkudo/actions"
- *          style="max-width:100%;" /></a>
- * </p>
- *
- * Why use this?
- *
- * - Consistancy with simplicity
- * - Automates creation of the action types by sniffing the action creators
- * - It works how you expect it to.  Changing a action type will also update action creator output.
- * - Automatically generates the success/failure actions for side effect actions
- *   (For libraries like [Redux-Thunk]{@link https://github.com/reduxjs/redux-thunk},
- *   [Redux-Saga]{@link https://redux-saga.js.org/}, [Redux-Observable]{@link https://redux-observable.js.org/}...)
- * - All action type strings are prefixed the namespace on contruction
- *
- * Install with:
- *
- * ```console
- * yarn add @zakkudo/actions
- * ```
- *
- * @example
- * import Actions from '@zakkudo/actions';
- *
- * const actions = new Actions({
- *     setValue(value) {
- *         return {
- *             type: 'SET_VALUE',
- *             value,
- *         };
- *     },
- *     requestValue(request) {
- *         return {
- *             type: 'REQUEST_VALUE',
- *             request,
- *         };
- *     }
- * }, 'APPLICATION');
- *
- * // Automatically generates the action type strings with a namespace
- *
- * actions.setValue(3); // {type: "@APPLICATION/SET_VALUE", value: 3}
- * actions.SET_VALUE // @APPLICATION/SET_VALUE
- *
- * actions.requestValue(() => fetch('/data'));
- * actions.REQUEST_VALUE // @APPLICATION/REQUEST_VALUE
- *
- * // The below are also automatically generated from requestValue() because
- * // it's an async action
- *
- * actions.valueRequestSucceeded(response); // {type: "@APPLICATION_VALUE_REQUEST_SUCCEEDED", response}
- * actions.VALUE_REQUEST_SUCCEEDED // @APPLICATION/VALUE_REQUEST_SUCCEEDED
- *
- * actions.valueRequestFailed(reason); // {type: "@APPLICATION/VALUE_REQUEST_FAILED", reason}
- * actions.VALUE_REQUEST_FAILED // @APPLICATION/VALUE_REQUEST_FAILED
- *
- * Object.keys(actions) // ['setValue', 'SET_VALUE',
- *                      //  'requestValue', 'REQUEST_VALUE',
- *                      //  'valueRequestSucceeded', 'VALUE_REQUEST_SUCCEEDED",
- *                      //  'valueRequestFailed', 'VALUE_REQUEST_FAILED']
- * @module Actions
- */
-export default class Actions {
+class Actions {
+    /**
+     * @param {Object} actionCreators - An object containing a set of
+     * [action creators]{@link https://redux.js.org/basics/actions#action-creators}
+     * to auto complete the types for.
+     * @param {String} [namespace] - The desired namespace for the set of actions
+     */
     constructor(actionCreators, namespace='') {
         Object.assign(this,
             addActionTypeNamesWithScopes(
@@ -196,3 +131,5 @@ export default class Actions {
         );
     }
 }
+
+export default Actions;
